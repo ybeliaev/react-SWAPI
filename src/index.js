e6809f013e01b19767d2fcff9d9fe704bf7e0dc7
Yura <ybeliaev@gmail.com>
2019-08-08 17:42:16 +0300

    async await adding

--- src/index.js
+++ src/index.js
@@ -3,13 +3,22 @@ import ReactDOM from "react-dom";
 
 import "./styles.css";
 
-fetch("https://swapi.co/api/people/1/")
-  .then(res => {
-    return res.json();
-  })
-  .then(body => {
-    console.log(body);
-  });
+const getResourse = async url => {
+  const res = await fetch(url);
+  const body = await res.json();
+  return body;
+};
+getResourse("https://swapi.co/api/people/1/").then(body => {
+  console.log(body);
+});
+
+// fetch("https://swapi.co/api/people/1/")
+//   .then(res => {
+//     return res.json();
+//   })
+//   .then(body => {
+//     console.log(body);
+//   });
 
 function App() {
   return (
