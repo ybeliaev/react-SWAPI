471d2d1c580277177f9e3b2b44e3703ff23b62ec
Yura <ybeliaev@gmail.com>
2019-08-09 18:13:54 +0300

    all names

--- src/index.js
+++ src/index.js
@@ -1,33 +1,44 @@
-import React from "react";
-import ReactDOM from "react-dom";
-
-import "./styles.css";
-
-class SwappiService {
-  async getResourse(url) {
-    const res = await fetch(url);
+class SwappiServise {
+  _apiBase = "https://swapi.co/api";
+  async getResource(url) {
+    const res = await fetch(`${this._apiBase}${url}`);
     if (!res.ok) {
       throw new Error("Error! URL: " + url + "Error status: " + res.status);
     }
 
     return await res.json();
   }
-  getAllPersons() {
-    return SwappiService(`http https://swapi.co/api/people`);
+  async getAllPersons() {
+    const res = await this.getResource(`/people/`);
+    return res.results;
   }
-  getPerson(id) {
-    return SwappiService(`http https://swapi.co/api/people/${id}`);
+  async getPerson(id) {
+    return this.getResourse(`/people/${id}`);
+  }
+  async getAllPlanets() {
+    const res = await this.getResource(`/planets/`);
+    return res.results.map(this._transformPlanet);
+  }
+
+  async getPlanet(id) {
+    const planet = await this.getResource(`/planets/${id}/`);
+    return this._transformPlanet(planet);
+  }
+
+  async getAllStarships() {
+    const res = await this.getResource(`/starships/`);
+    return res.results.map(this._transformStarship);
+  }
+
+  async getStarship(id) {
+    const starship = this.getResource(`/starships/${id}/`);
+    return this._transformStarship(starship);
   }
 }
+const swapi = new SwappiServise();
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Hello CodeSandbox!!!</h1>
-      <h2>Start editing to see some magic happen!</h2>
-    </div>
-  );
-}
-
-const rootElement = document.getElementById("root");
-ReactDOM.render(<App />, rootElement);
+swapi.getAllPersons().then(people => {
+  people.forEach(el => {
+    console.log(el.name);
+  });
+});
