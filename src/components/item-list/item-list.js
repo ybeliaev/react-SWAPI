97e0e512258524c4b28182c791ae92afef37e499
Yura <ybeliaev@gmail.com>
2019-08-19 15:35:32 +0300

    componentDidMount in item-list

--- src/components/item-list/item-list.js
+++ src/components/item-list/item-list.js
@@ -1,8 +1,21 @@
 import React, { Component } from "react";
 
 import "./item-list.css";
+import SwapiService from "../../services/swapi-service";
 
 export default class ItemList extends Component {
+  swapiService = new SwapiService();
+  state = {
+    peoplList: null
+  };
+  componentDidMount() {
+    this.swapiService.getAllPeople().then(peoplList => {
+      this.setState({
+        peoplList
+      });
+    });
+  }
+
   render() {
     return (
       <ul className="item-list list-group">
